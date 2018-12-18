fee2f443e4946d6c2a6dca741d3e88e5bc4f1ced
HP Boaz <boazfloor@gmail.com>
2018-12-18 16:34:10 +0100

    remove debug comments

--- lmpcc/src/lmpcc_controller.cpp
+++ lmpcc/src/lmpcc_controller.cpp
@@ -410,7 +410,7 @@ void LMPCC::controlLoop(const ros::TimerEvent &event)
 		        goal_reached_ = true;
                 ROS_ERROR_STREAM("GOAL REACHED");
 
-                for (int vv_it = 0; vv_it < 5; vv_it ++){std::cout << "vv: " << vv[vv_it] << std::endl;}
+//                for (int vv_it = 0; vv_it < 5; vv_it ++){std::cout << "vv: " << vv[vv_it] << std::endl;}
                 if (loop_mode_)
                 {
                     segment_counter = 0;
@@ -429,7 +429,7 @@ void LMPCC::controlLoop(const ros::TimerEvent &event)
                 acadoVariables.x[3] = referencePath.GetS0();
                 ROS_ERROR_STREAM("SWITCH SPLINE, segment_counter =  " << segment_counter);
 
-                for (int vv_it = 0; vv_it < 5; vv_it ++){std::cout << "vv: " << vv[vv_it] << std::endl;}
+//                for (int vv_it = 0; vv_it < 5; vv_it ++){std::cout << "vv: " << vv[vv_it] << std::endl;}
 		    }
         }
 
